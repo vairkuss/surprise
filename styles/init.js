3e0ddccfff2436892dd61f48c47582b9d6a4453e
vairkuss <vairkuss.work@gmail.com>
2026-08-26 13:40:47 +0200

    icon grad fix / text wip

--- styles/init.js
+++ styles/init.js
@@ -3,16 +3,50 @@ class CompletePage {
         const headElement = document.querySelector("head");
         headElement.insertAdjacentHTML(
             "beforeend",
-            ["colorctl", "textctl", "bgctl", "blockctl", "buttonctl"]
-                .map(v => `<link rel="stylesheet" href="${v}.css" />`)
+            ["colorctl", "textctl", "bgctl", "blockctl", "buttonctl", "debugctl"]
+                .map(ctl => `<link rel="stylesheet" href="${ctl}.css" />`)
                 .concat([
                     '<meta charset="UTF-8"/>'
                 ])
                .join("")
         );
     }
+    
+    static #addCommonInsertion () {
+        document.querySelector("body").insertAdjacentHTML("beforeend", `
+<div class="block" id="input" style="opacity:0">
+    <input type="text" id="values" placeholder="h, s, l" value="714, 825, 285" />
+    <pre id="message"></pre>
+    <div class="btn block" id="execute" onclick="newColor()">
+        <div class="icon">submit</div>
+    </div>
+</div>
+`);
+        const grads = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        grads.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        grads.setAttribute("id", "grads");
+        grads.innerHTML = `
+<defs>
+    <linearGradient id="text-grad" x1="37%" y1="2%" x2="63%" y2="98%">
+        <stop offset="0%" stop-color="hsl(calc(var(--m-hue) - 5), calc(var(--m-sat) * 0.8), calc(var(--m-lum) * 0.75))" />
+        <stop offset="25%" stop-color="hsl(var(--m-hue), calc(var(--m-sat) * 0.8), calc(var(--m-lum) * 0.6))" />
+        <stop offset="50%" stop-color="hsl(calc(var(--m-hue) + 5), calc(var(--m-sat) * 0.8), calc(var(--m-lum) * 0.75))" />
+        <stop offset="75%" stop-color="hsl(var(--m-hue), calc(var(--m-sat) * 0.8), calc(var(--m-lum) * 0.9))" />
+        <stop offset="100%" stop-color="hsl(calc(var(--m-hue) - 5), calc(var(--m-sat) * 0.8), calc(var(--m-lum) * 0.75))" />
+    </linearGradient>
+    <linearGradient id="dis-grad" x1="37%" y1="2%" x2="63%" y2="98%">
+        <stop offset="0%" stop-color="hsl(calc(var(--m-hue) - 5), calc(var(--m-sat) * 0.1), calc(var(--m-lum) * 0.55))" />
+        <stop offset="25%" stop-color="hsl(var(--m-hue), calc(var(--m-sat) * 0.05), calc(var(--m-lum) * 0.4))" />
+        <stop offset="50%" stop-color="hsl(calc(var(--m-hue) + 5), calc(var(--m-sat) * 0.1), calc(var(--m-lum) * 0.55))" />
+        <stop offset="75%" stop-color="hsl(var(--m-hue), calc(var(--m-sat) * 0.05), calc(var(--m-lum) * 0.7))" />
+        <stop offset="100%" stop-color="hsl(calc(var(--m-hue) - 5), calc(var(--m-sat) * 0.1), calc(var(--m-lum) * 0.55))" />
+    </linearGradient>
+</defs>
+`;
+        document.querySelector("body").appendChild(grads);
+    }
 
-    static #developBlocks() {
+    static #developBlocks () {
         [...document.querySelectorAll(".block")]
             .filter(
                 blockElement => ![...blockElement.children].flatMap(
@@ -37,7 +71,7 @@ class CompletePage {
         ) { this.#developBlocks() }
     }
 
-    static #loadSVG() {
+    static #loadSVG () {
     
         document.querySelectorAll(".icon").forEach(
             async iconElement => {
@@ -49,10 +83,8 @@ class CompletePage {
                     if (iconElement.textContent) {
                         const iconStyle = window.getComputedStyle(iconElement);
                         iconElement.innerHTML = `
-<svg xmlns="http://www.w3.org/2000/svg" width="72vw" height="var(--icon-size)">
-    <text x="50%" y="50%" class="text">
-        ${iconElement.textContent}
-    </text>
+<svg class="text" viewBox="0 0 250 250" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+    <text x="125" y="125" text-anchor="middle" font-size="${iconStyle.fontSize}" vector-effect="non-scaling-size">${iconElement.textContent}</text>
 </svg>
 `;
                     }
@@ -80,16 +112,77 @@ class CompletePage {
             }
         );
     }
+    
+    static #breakLines() {
+        document.querySelectorAll(".text").forEach(
+            async textElement => {
+                let text = textElement.textContent.split(" ");
+                const maxWidth = window.getComputedStyle(textElement.parentElement).width;
+                console.log(textElement.textContent, maxWidth);
+                let string = [];
+                
+            }
+        )
+    }
 
-    static #ready = 0;
+    static #used = 0;
 
-    static init() {
-        if (this.#ready) { return }
+    static init () {
+        if (this.#used) { return }
+        this.#used = 1;
         this.#addMeta();
+        this.#addCommonInsertion();
         this.#developBlocks();
         this.#loadSVG();
-        this.#ready = 1;
+        this.#breakLines();
+        document.querySelector("#cover").style.setProperty("height", "0");
     }
+}
+
+
+function getInputValues () { 
+    const msgElement = document.querySelector("#message");
+    const inputElement = document.querySelector("#values");
+    try {
+        msgElement.innerText = "";
+        let values = inputElement.value.split(",")
+            .map(v => parseInt(v) ? parseInt(v) : 0);
+        if (values.length !== 3) { 
+            throw new Error(
+`h, s, l parameters must be just numbers
+and properly separated with commas
+expected: 3
+got:      ${values.length}`
+            );
+        }
+        values[0] %= 360;
+        values[1] = Math.max(5, Math.min(values[1], 100));
+        values[2] = Math.max(5, Math.min(values[2], 100));
+        return values;
+    } catch (e) {
+        msgElement.innerText = e.toString();
+        console.error(e);
+    }
+}
+
+function newColor () {
+    const input = getInputValues();
+    if (input != null) {
+        const [h, s, l] = input;
+        const root = document.querySelector(":root");
+        root.style.setProperty('--m-hue', `${h}`);
+        root.style.setProperty('--m-sat', `${s}%`);
+        root.style.setProperty('--m-lum', `${l}%`);
+    }
+}
+
+
+let debugModeOn = -1;
+function debug() {
+    const inputElement = document.querySelector("#input");
+    debugModeOn *= -1;
+    inputElement.style.setProperty("transform", debugModeOn > 0 ? "scale(100%)" : "scale(0)");
+    inputElement.style.setProperty("opacity", debugModeOn > 0 ? "100%" : "0");
 }
 
 /**
